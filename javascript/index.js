c33591a09df416c5f9225140d6cddc310f2f151a
Anmol Agrawal <anmolagrawal873@gmail.com>
2022-05-26 09:11:00 +0530

    this is commit changes in index and javascript file

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,26 @@
 console.log("This is notes Taking Application ");
+
+// if user added the notes then aaded it inot local Storage
+
+
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click",function(e){
+    console.log("Clicked on add note butn");
+    let notesHeading = document.getElementById("addHeading");
+    let notesContent = document.getElementById("addContent");
+    let notes = localStorage.getItem("notes");
+    let notesObj;
+    if(notes == null)
+    {
+         notesObj = [];
+    }
+    else
+    {
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.push(notesHeading.value);
+    localStorage.setItem("notes",JSON.stringify(notesObj));
+    console.log(notesHeading); 
+    notesHeading.value = '';
+    console.log(notes);
+})
